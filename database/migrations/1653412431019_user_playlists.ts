0e69f8fd66d8c2747a14089bf41886b05a66edf7
Anthony <anthonynwobodo7@gmail.com>
2022-05-25 01:18:56 +0100

    :sparkles: (defined new models)

---
+++ database/migrations/1653412431019_user_playlists.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class UserPlaylists extends BaseSchema {
+  protected tableName = 'userPlaylists'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('createdAt', { useTz: true })
+      table.timestamp('updatedAt', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
