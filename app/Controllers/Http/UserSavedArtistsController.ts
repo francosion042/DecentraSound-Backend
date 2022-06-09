7514fc45a8043364dc64b16ff6f1a9472f9a6c5f
Anthony <anthonynwobodo7@gmail.com>
2022-06-09 21:16:48 +0100

    :ambulance: (preload)

--- app/Controllers/Http/UserSavedArtistsController.ts
+++ app/Controllers/Http/UserSavedArtistsController.ts
@@ -11,7 +11,7 @@ export default class UserSavedArtistsController {
       .preload('artist', (artist) => {
         artist
           .preload('albums', (album) => {
-            album.preload('genre').preload('songs')
+            album.preload('genre').preload('songs').preload('artist')
           })
           .preload('songs')
       })
